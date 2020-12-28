6aab6a816fb9343c5a28c6deb65c18ee6e9bccb8
afilonczuk <57341106+afilonczuk@users.noreply.github.com>
2020-12-28 10:37:03 -0500

    Add files via upload

--- MIRT.R
+++ MIRT.R
@@ -1,5 +1,59 @@
 library(mirt)
 
+
+########### Function for estimating parameters ##############
+
+par.est<- function(a, d, c, thetas, out, probmatr, dif, sqbias){
+  
+  for (i in 1:l){ #looping over respondents
+    #calculate the value in the exponent (xp) for one respondent
+    xp <- a%*%thetas[,i]+d
+    #find the probability that someone answers "correctly" for each of the items 
+    #(should result in a mx1 vector of probabilities; m number of items for 1 participent) 
+    #add this as a row to a matrix of probabilities "probmatr"
+    probmatr[i,]<- 1/(1+exp(-1*xp))
+    
+    for (j in 1:nrow(d)){ #looping over number of items
+      #generate outcomes based on the probabilities from the probability matrix using
+      #  the binomial distribution; each probability gets replaced with either a 0 or a 1
+      out[i,j]<-rbinom(n=1, size = 1, prob = probmatr[i,j]) 
+    }
+  }
+  #Recover parameters using the data generated in "out" and m number of dimensions
+  #obtain the starting values before we fix & constrain values
+  sv<- try(mirt(out, model = 2, pars = 'values', itemtype = '2PL', TOL = 0.0005))
+  if (inherits(sv, "try-error")) {
+    count = count+1
+  }else{
+    #fix the starting values to 1.0
+    sv$value[sv$name=="a1"]<-sv$value[sv$name=="a2"]<- 1.0
+    #fix the first value to 0.0 
+    sv$value[1]<- 0.0
+    #set estimation of first value to false so that we will not be estimating it further
+    sv$est[1]<- F
+    #set the lower bounds for the "a" parameters 
+    sv$lbound[sv$name == "a1"] <- sv$lbound[sv$name == "a2"]<- 0.5
+    #set the upper bounds for the "a" parameters  
+    sv$ubound[sv$name == "a1"] <- sv$ubound[sv$name == "a2"]<- 1.5
+    
+    # estimate parameters given the new parameter specifications
+    new<- try(mirt(out, model = 2, pars = sv, itemtype = '2PL'))
+    
+    if (inherits(new, "try-error")) {
+      count = count+1
+    }else{ 
+      #if no error occurs, extract the parameters and bind them in ipar.est
+      #(note:this code is written for 10 items and must be revised if the number of items is changed)
+      ipar.est<- coef(new, simplify=TRUE)$items
+      #add the bias to update the matrix "dif" 
+      dif<- dif + ipar.est[, 1:3]-cbind(a,d) 
+      #add up the squared bias for rmse later
+      sqbias<- sqbias + (ipar.est[, 1:3]-cbind(a,d))^2
+      
+    }}
+  return(list("sqbias" = sqbias, "dif" = dif))
+}
+
 ###### Initialize Variables For Simulation ####################
 
 #m = number of dimensions for our mirt model
@@ -19,7 +73,6 @@ dif <- sqbias<- matrix(0, nrow = n, ncol = m+1)
 
 
 ############## Begin Simulation ########################
-#Note: if you change the number of items, you will have to add/remove coefficients in line 78 
 
 for (k in 1:trials){ #loop over each trial
   #a = our discrimination parameters; each column is for each dimension, each row is per item
@@ -34,67 +87,25 @@ for (k in 1:trials){ #loop over each trial
   probmatr <- out<- matrix(NA, nrow = l, ncol = nrow(d))
   # must name each column for the mirt function later (a unique name for each item)
   colnames(out)<-  paste0("Item.", c(1:nrow(d)))
+  #generate thetas: each row is a different dimension; each column is a different respondent
+  thetas<- matrix(data = rnorm(m*l,0,1), nrow=m, ncol = l)
 
-#generate thetas: each row is a different dimension; each column is a different respondent
-thetas<- matrix(data = rnorm(m*l,0,1), nrow=m, ncol = l)
+  final<- par.est(a, d, c, thetas, out, probmatr, dif, sqbias)
+  
+  
 
-  for (i in 1:l){ #looping over respondents
-    #calculate the value in the exponent (xp) for one respondent
-    xp <- a%*%thetas[,i]+d
-    #find the probability that someone answers "correctly" for each of the items 
-      #(should result in a mx1 vector of probabilities; m number of items for 1 participent) 
-      #add this as a row to a matrix of probabilities "probmatr"
-    probmatr[i,]<- 1/(1+exp(-1*xp))
-    
-  for (j in 1:nrow(d)){ #looping over number of items
-    #generate outcomes based on the probabilities from the probability matrix using
-    #  the binomial distribution; each probability gets replaced with either a 0 or a 1
-    out[i,j]<-rbinom(n=1, size = 1, prob = probmatr[i,j]) 
-  }
-  }
-
-#Recover parameters using the data generated in "out" and m number of dimensions
-  #obtain the starting values before we fix & constrain values
-sv<- try(mirt(out, model = 2, pars = 'values', itemtype = '2PL', TOL = 0.0005))
-if (inherits(sv, "try-error")) {
-  count = count+1
-}else{
-  #fix the starting values to 1.0
-sv$value[sv$name=="a1"]<-sv$value[sv$name=="a2"]<- 1.0
-  #fix the first value to 0.0 
-  sv$value[1]<- 0.0
-#set estimation of first value to false so that we will not be estimating it further
-sv$est[1]<- F
-#set the lower bounds for the "a" parameters 
-sv$lbound[sv$name == "a1"] <- sv$lbound[sv$name == "a2"]<- 0.5
-#set the upper bounds for the "a" parameters  
-sv$ubound[sv$name == "a1"] <- sv$ubound[sv$name == "a2"]<- 1.5
-
-# estimate parameters given the new parameter specifications
-new<- try(mirt(out, model = 2, pars = sv, itemtype = '2PL'))
-
-  if (inherits(new, "try-error")) {
-    count = count+1
-  }else{ 
-    #if no error occurs, extract the parameters and bind them in ipar.est
-        #(note:this code is written for 10 items and must be revised if the number of items is changed)
-    ipar.est<- coef(new, simplify=TRUE)$items
-    #add the bias to update the matrix "dif" 
-    dif<- dif + ipar.est[, 1:3]-cbind(a,d) 
-    #add up the squared bias for rmse later
-    sqbias<- sqbias + (ipar.est[, 1:3]-cbind(a,d))^2
-    
-  }}
 }
 #end simulation
 
+
+################ Final Results ######################
 #the average RMSE for each item
-rmse <- colSums(sqrt(sqbias/trials))/n
+rmse <- colSums(sqrt(final$sqbias/trials))/n
 rmse
 #the average bias for each parameter per item
-write.table(round(dif/(trials), 4))
+write.table(round(final$dif/(trials), 4))
 #the average bias for each parameter averaged over the items
-write.table(round(colSums(dif/(trials))/n, 4))
+write.table(round(colSums(final$dif/(trials))/n, 4))
 
 
 
